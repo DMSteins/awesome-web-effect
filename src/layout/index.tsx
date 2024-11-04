8cb7e2df74521db19471a493b817ed797d5df30a
DMSteins <dmhongcode@gmail.com>
2024-11-04 23:31:53 +0800

    init

---
+++ src/layout/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Header from './header'
+import Footer from './footer'
+
+export default ()=>{
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
