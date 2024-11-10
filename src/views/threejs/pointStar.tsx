a9995b6a3bfaa8b88623839630e233d5424f52fe
DMSteins <dmhongcode@gmail.com>
2024-11-10 23:29:44 +0800

    feat: linear color

--- src/views/threejs/pointStar.tsx
+++ src/views/threejs/pointStar.tsx
@@ -15,9 +15,9 @@ export default () => {
 	const renderBoxRef = useRef<HTMLDivElement>(null)
 	const render = () => {
 		if(!scene.current) return
-		const time = clock.current!.getElapsedTime() * 0.5;
+		const time = clock.current!.getElapsedTime() * 0.3;
 		sphereMaterial.current!.uniforms.uTime.value = time
-		// sphere.current!.rotation.y = time;
+		sphere.current!.rotation.y = time;
 		renderer.current!.render(scene.current!, camera.current!);
 		requestAnimationFrame(render);
 	}
@@ -42,27 +42,39 @@ export default () => {
 
 		controls.current = new OrbitControls(camera.current, renderer.current.domElement);
 
-		const sphereGeometry = new THREE.IcosahedronGeometry(10, 6);
+		
 
 		const vertexShader = /* GLSL */ `
   uniform float uTime;
-  varying vec2 vUv;
+//   varying vec2 vUv;
+  varying vec3 vColor;
 
   void main() {
 
-    vUv = uv;
+    // vUv = uv;
+
+	vec3 color1 = vec3(227., 155., 0.);
+  	vec3 color2 = vec3(100., 50., 255.);
+
+	float d = position.y / 10.0 * 0.5 + 0.5;
+	vColor = mix(color1, color2, d) / 255.0;
     
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = 100.0 / -mvPosition.z;
+	// gl_PointSize = 10.0;
+    gl_PointSize = 60.0 / -mvPosition.z;
     gl_Position = projectionMatrix * mvPosition;
   }
 `;
 
 		const fragmentShader = /* GLSL */ `
-  varying vec2 vUv;
+//   varying vec2 vUv;
+   varying vec3 vColor;
   void main() {
-    float color = step(0.5, vUv.x);
-    gl_FragColor = vec4(fract(vUv.x * 3.0), 0.0, 0.0, 1.0);
+    // float color = step(0.5, vUv.x);
+    // gl_FragColor = vec4(fract(vUv.x * 3.0), 0.0, 0.0, 1.0);
+	float mask = step(length(gl_PointCoord - 0.5), 0.5);
+  	if(mask < 0.5) discard;
+	gl_FragColor = vec4(vColor, 1.);
   }
 `;
 
@@ -72,9 +84,22 @@ export default () => {
 			uniforms: {
 				uTime: { value: 0 },
 			},
-			wireframe: true,
+			// wireframe: true,
 		});
 		// sphere.current = new THREE.Mesh(sphereGeometry, sphereMaterial.current);
+		// const sphereGeometry = new THREE.IcosahedronGeometry(10, 6);
+		const pos = []
+		for (let i = 0; i < 10000; i++) {
+			const rad = 9.5 + 0.5 * Math.random()
+			const theta = Math.random() * Math.PI * 2;
+			const angle = Math.PI * 2 * Math.random()
+			const x = rad * Math.sin(angle) * Math.cos(theta)
+			const y = rad * Math.cos(angle)
+			const z = rad * Math.sin(angle) * Math.sin(theta)
+			pos.push(x, y, z)
+		}
+		const sphereGeometry = new THREE.BufferGeometry()
+		sphereGeometry.setAttribute("position", new THREE.Float32BufferAttribute(pos, 3))
 		sphere.current = new THREE.Points(sphereGeometry, sphereMaterial.current)
 		scene.current.add(sphere.current);
 
