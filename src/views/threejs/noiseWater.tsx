6541a0350ea56b6e0bb8bda42eba057907e33eac
DMSteins <dmhongcode@gmail.com>
2024-11-19 22:15:31 +0800

    feat: fire start bg

--- src/views/threejs/noiseWater.tsx
+++ src/views/threejs/noiseWater.tsx
@@ -11,12 +11,14 @@ export default ()=>{
 	const material = useRef<THREE.ShaderMaterial>()
 	const mesh = useRef<THREE.Mesh>()
 	const particleMaterial = useRef<THREE.ShaderMaterial>()
+	const fireMaterial = useRef<THREE.ShaderMaterial>()
 	const renderBoxRef = useRef<HTMLDivElement>(null)
 	const render = () => {
 		if(!scene.current) return
 		const time = clock.current!.getElapsedTime();
 		material.current!.uniforms.uTime.value = time
 		particleMaterial.current!.uniforms.uTime.value = time
+		fireMaterial.current!.uniforms.uTime.value = time
 		mesh.current!.rotation.y = time * 0.3;
 		renderer.current!.render(scene.current!, camera.current!);
 		requestAnimationFrame(render);
@@ -101,6 +103,62 @@ export default ()=>{
 		const particleMesh = new THREE.Points(particleGeometry, _particleMaterial)
 		_scene.add(particleMesh)
 		particleMaterial.current = _particleMaterial
+
+		const firefliesGeometry = new THREE.BufferGeometry();
+		const firefliesCount = 300;
+		const positions1 = new Float32Array(firefliesCount * 3);
+		const sizes = new Float32Array(firefliesCount);
+
+		for (let i = 0; i < firefliesCount; i++) {
+			const r = Math.random() * 5 + 5;
+			positions1[i * 3 + 0] = (Math.random() - 0.5) * r;
+			positions1[i * 3 + 1] = (Math.random() - 0.5) * r;
+			positions1[i * 3 + 2] = (Math.random() - 0.5) * r;
+
+			sizes[i] = Math.random() + 0.4;
+		}
+
+		firefliesGeometry.setAttribute(
+		"position",
+		new THREE.BufferAttribute(positions1, 3)
+		);
+		firefliesGeometry.setAttribute("aSize", new THREE.BufferAttribute(sizes, 1));
+
+		const firefliesVertexShader = /* GLSL */ `
+			uniform float uTime;
+			attribute float aSize;
+
+			void main() {
+				vec3 newPos = position;
+				newPos.y += sin(uTime * 0.5 + newPos.x * 100.0) * aSize * 0.2;
+				newPos.x += sin(uTime * 0.5 + newPos.x * 200.0) * aSize * 0.1;
+				vec4 mvPosition = modelViewMatrix * vec4(newPos, 1.0);
+				gl_PointSize = 70.0 * aSize / -mvPosition.z;
+				gl_Position = projectionMatrix * mvPosition;
+			}
+		`;
+
+		const firefliesFragmentShader = /* GLSL */ `
+			void main() {
+			float d = length(gl_PointCoord - vec2(0.5));
+			float strength = clamp(0.05 / d - 0.05 * 2.0, 0.0, 1.0);
+			gl_FragColor = vec4(vec3(1.0), strength);
+		}
+		`;
+		const _fireMaterial = new THREE.ShaderMaterial({
+			vertexShader: firefliesVertexShader,
+			fragmentShader: firefliesFragmentShader,
+			uniforms: {
+				uTime: { value: 0 },
+			},
+			transparent: true,
+			blending: THREE.AdditiveBlending,
+			depthWrite: false,
+		})
+		fireMaterial.current = _fireMaterial
+		const fireMesh = new THREE.Points(firefliesGeometry, _fireMaterial)
+
+		_scene.add(fireMesh)
 
 		const _renderer = new THREE.WebGLRenderer()
 		_renderer.setPixelRatio(window.devicePixelRatio);
